c3fcff74195a5d4cd7542dbf0439df3f34f6e481
William Felker <gndclouds@icloud.com>
2021-01-30 17:31:40 +0800

    Base for Glitch

---
+++ app.js
@@ -0,0 +1,58 @@
+const { Client, APIMessage } = require("discord.js");
+const { readdirSync } = require("fs");
+const axios = require("axios");
+const qs = require("qs");
+const MongoClient = require("mongodb").MongoClient;
+const Bot = new Client();
+const Config = require("./config");
+const Commands = [];
+const cmdFiles = readdirSync("./commands").filter((file) =>
+	file.endsWith(".js"),
+);
+
+Bot.on("ready", async () => {
+	for (const fileName of cmdFiles) {
+		const File = require(`./commands/${fileName}`);
+		Commands.push(File);
+		await Bot.api.applications(Bot.user.id).commands.post({
+			data: {
+				name: File.name,
+				description: File.description,
+				options: File.options,
+			},
+		});
+	}
+	console.info(`Logged in as ${Bot.user.username}`);
+	console.info(`Ready for 📦`);
+});
+
+Bot.ws.on("INTERACTION_CREATE", (interaction) => {
+	const CMDFile = Commands.find(
+		(cmd) => cmd.name.toLowerCase() === interaction.data.name.toLowerCase(),
+	);
+	if (CMDFile)
+		CMDFile.execute(Bot, say, interaction, interaction.data.options);
+});
+
+Bot.login(Config.token);
+
+async function say(interaction, content) {
+	return Bot.api
+		.interactions(interaction.id, interaction.token)
+		.callback.post({
+			data: {
+				type: 4,
+				data: await createAPIMessage(interaction, content),
+			},
+		});
+}
+
+async function createAPIMessage(interaction, content) {
+	const apiMessage = await APIMessage.create(
+		Bot.channels.resolve(interaction.channel_id),
+		content,
+	)
+		.resolveData()
+		.resolveFiles();
+	return { ...apiMessage.data, files: apiMessage.files };
+}
---
+++ commands/drafts/share.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+const qs = require("qs");
+
+module.exports = {
+	name: "share",
+	description: "tell us about somethings you want to share on the tiny factores twitter or newsletter",
+	options: [
+		{
+			name: "Post",
+			description: "Sends this post to tiny Garden's Public page",
+			type: 3,
+			required: true,
+		},
+	],
+	async execute(_bot, say, interaction, args) {
+		console.info(args);
+		var data = JSON.stringify(args);
+
+		var config = {
+			method: "post",
+			url: "https://tiny-garden-server.glitch.me/api/post",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: data,
+		};
+		axios(config)
+			.then(function (response) {})
+			.catch(function (error) {
+				console.log(error);
+			});
+	},
+};
